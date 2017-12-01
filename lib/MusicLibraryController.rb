c0636dc4874f578e19b2f738d3d068ffe9859148
Miwa <paintfx@gmail.com>
2017-12-01 14:54:39 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -9,33 +9,33 @@ class MusicLibraryController
     input = ""
 
     while input != 'exit'
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    input = gets.strip
+      input = gets.strip
 
-    case input
-    when 'list songs'
-      list artists
-    when 'list artists'
-      list genres
-    when 'list genres'
-      list songs
-    when 'list artist'
-      list_songs_by_artist
-    when 'list genre'
-      list songs_by_genre
-    when 'play song'
-      play song
-    
-
+      case input
+      when 'list songs'
+        list artists
+      when 'list artists'
+        list genres
+      when 'list genres'
+        list songs
+      when 'list artist'
+        list_songs_by_artist
+      when 'list genre'
+        list songs_by_genre
+      when 'play song'
+        play song
+      end    
+    end
   end
 
 end
