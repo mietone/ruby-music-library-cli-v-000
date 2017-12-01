552396ab31458fad710ba67c5898b5c9362977d2
Miwa <paintfx@gmail.com>
2017-12-01 19:14:54 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -76,6 +76,8 @@ class MusicLibraryController
     if data = Genre.find_by_name(input)
       data.songs.sort{|a, b| a.name.downcase <=> b.name.downcase}.each.with_index(1) do |s, i|
         puts "#{i}. #{s.name} - #{s.artist.name}"
+      end
+    end
   end
 
 end
