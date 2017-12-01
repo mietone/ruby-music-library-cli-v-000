60d085ae7fe81b1465fef61d548680b81a8b4805
Miwa <paintfx@gmail.com>
2017-12-01 19:20:13 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -81,7 +81,7 @@ class MusicLibraryController
   end
 
   def play_song
-    puts "Please enter the name of a song:"
+    puts "Which song number would you like to play?"
     input = gets.strip
 
     # if data = Song.find_by_name(input)
