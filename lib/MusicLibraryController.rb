6e7b475ffd0ba68b4c0b89f770fefab97c8f71fe
Miwa <paintfx@gmail.com>
2017-12-01 19:15:57 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -70,7 +70,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre
-    puts "Please enter a genre"
+    puts "Please enter the name of a genre:"
     input = gets.strip
 
     if data = Genre.find_by_name(input)
