aacf566fe64a04dbbfc9f6ded659a7bcf2068679
Miwa <paintfx@gmail.com>
2017-12-01 16:43:36 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -41,8 +41,8 @@ class MusicLibraryController
 
 
   def list_songs
-    Song.all.sort{|a, b| a.name.downcase <=> b.name.downcase}.each.with_index(1) do |song, index|
-    puts "#{index}. #{name} - #{song} - #{genre}"
+    Song.all.sort{|a, b| a.name.downcase <=> b.name.downcase}.each.with_index(1) do |s, i|
+    puts "#{i}. #{s.aritst.name} - #{s.name} - #{s.genre.name}"
   end
   # binding.pry
 
