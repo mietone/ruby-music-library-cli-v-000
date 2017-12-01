62b3ce8ae82784b50a70dd4601e7679768b93792
Miwa <paintfx@gmail.com>
2017-12-01 19:29:51 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -85,10 +85,10 @@ class MusicLibraryController
     input = gets.strip.to_i
 
     if (1..Song.all.length).include?(input)
-      choice = Song.all.sort{|a, b| a.name.downcase <=> b.name.downcase}[input - 1]
+      song = Song.all.sort{|a, b| a.name.downcase <=> b.name.downcase}[input - 1]
     end
 
-    puts "Playing #{choice.name} by #{choice.artist.name}"
+    puts "Playing #{song.name} by #{song.artist.name}"
 
   end
 
