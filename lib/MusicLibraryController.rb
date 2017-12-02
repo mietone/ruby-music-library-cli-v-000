678c731fb4c1e195db415f8abd3139c60d7062ea
Miwa <paintfx@gmail.com>
2017-12-02 01:17:24 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -87,7 +87,7 @@ class MusicLibraryController
     if input.between?(1, Song.all.length)
       song = Song.all.sort_by{|s| s.name}[input - 1]
 
-      puts "Playing #{song.name} by #{song.artist.name}"
+      puts "Playing #{song.name} by #{song.artist.name}" if song
     end
   end
 
